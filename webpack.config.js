60474e65a3287f05d9ec3cc63e7d3978745cae23
Pierre de Lacroix <pierre@pdelacroix.com>
2020-03-08 19:59:39 +0100

    Use HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
     mode: "development",
     entry: "./lib/es6/src/main.bs.js",
     output: {
-        publicPath: __dirname+'/dist',
+        publicPath: '/dist/',
         path: __dirname+'/dist',
         filename: "[name].js"
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: './index.html',
+            template: './src/index.ejs'
+        })
+    ],
     devtool: "source-map",
     devServer: {
         //allowedHosts: ['imago-dev.img', 'localhost'],
-        contentBase: __dirname,
+        contentBase: './dist',
         disableHostCheck: true,
         //headers: {
         //    'Access-Control-Allow-Origin': '*',
