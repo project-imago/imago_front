eb511226bb484558782db86291db83bacd2de826
Pierre de Lacroix <pierre@pdelacroix.com>
2020-04-19 00:08:55 +0200

    use routes without hash

--- src/router.ml
+++ src/router.ml
@@ -7,20 +7,21 @@ type route =
   | Room of Matrix.room_id
 
 let route_of_location location =
-  let route = Js.String.split "/" location.Web.Location.hash in
+  let route = Js.String.split "/" location.Web.Location.pathname in
+  (* let () = Js.log route in *)
   match route with
-  | [|"#"; "login"|] -> Login
-  | [|"#"; "signup"|] -> Signup
-  | [|"#"; "logout"|] -> Logout
-  | [|"#"; "group"; "new"|] -> CreateGroup
-  | [|"#"; "room"; room_id|] -> Room room_id
+  | [|""; "login"|] -> Login
+  | [|""; "signup"|] -> Signup
+  | [|""; "logout"|] -> Logout
+  | [|""; "group"; "new"|] -> CreateGroup
+  | [|""; "room"; room_id|] -> Room room_id
   | _ -> Index  (* default route *)
 
 let location_of_route = function
-  | Index -> "#/"
-  | Login -> "#/login"
-  | Signup -> "#/signup"
-  | Logout -> "#/logout"
-  | CreateGroup -> "#/group/new"
-  | Room room_id -> Printf.sprintf "#/room/%s" room_id
+  | Index -> "/"
+  | Login -> "/login"
+  | Signup -> "/signup"
+  | Logout -> "/logout"
+  | CreateGroup -> "/group/new"
+  | Room room_id -> Printf.sprintf "/room/%s" room_id
 
