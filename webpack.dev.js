9803e8216708b81017af322c93564929bc5b3093
Pierre de Lacroix <pierre@pdelacroix.com>
2020-06-26 21:40:25 +0200

    production mode 2

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,29 +2,29 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
+    mode: 			'development',
+    devtool: 			'inline-source-map',
     devServer: {
-        //allowedHosts: ['imago-dev.img', 'localhost'],
-        contentBase: './dist',
-        disableHostCheck: true,
-        //headers: {
-        //    'Access-Control-Allow-Origin': '*',
-        //},
-        host: '0.0.0.0',
-        hot: true,
-        //http2: true,
-        //https: true,
-        //inline: true,
-        liveReload: false,
-        port: 9000,
-        //public: 'imago-dev.img',
-        publicPath: '/dist/',
+        contentBase: 		'./dist',
+        disableHostCheck: 	true,
+        host: 			'0.0.0.0',
+        hot: 			true,
+        liveReload: 		false,
+        port: 			9000,
+        publicPath: 		'/dist/',
         historyApiFallback: {
-            disableDotRule: true
+            disableDotRule: 	true
         },
-        //sockHost: 'imago-dev.img',
-        //useLocalIp: false,
-        writeToDisk: true
+        writeToDisk: 		true
+        // allowedHosts: ['imago-dev.img', 'localhost'],
+        // headers: {
+        //     'Access-Control-Allow-Origin': '*',
+        // },
+        // http2: true,
+        // https: true,
+        // inline: true,
+        // public: 'imago-dev.img',
+        // sockHost: 'imago-dev.img',
+        // useLocalIp: false,
     }
 });
