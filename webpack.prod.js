321a75caa744e26689d4b1a22e9da09b0f6c2a2c
Pierre de Lacroix <pierre@pdelacroix.com>
2020-06-29 04:23:48 +0200

    production mode 3

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,27 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const ClosurePlugin = require('closure-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
     output: {
 	publicPath: '/'
     },
+    optimization: {
+	minimizer: [
+	    new ClosurePlugin({mode: 'STANDARD', childCompilations: true}, {
+	    })
+	],
+	splitChunks: { chunks: "all" }
+    },
     plugins: [
         new webpack.EnvironmentPlugin({
             NODE_ENV: 'production',
             MATRIX_URL: 'https://matrix.alpha.imago.pm',
             API_URL: 'https://alpha.imago.pm'
-        })
+        }),
+	new CompressionPlugin()
     ]
 });
